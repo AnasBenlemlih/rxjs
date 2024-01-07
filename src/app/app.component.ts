1f0a1b1659d5de931566de5a60257500d4e7129b
anas benlemlih <anas.benlemlih@gmail.com>
2024-01-08 00:19:40 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,23 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'rxjs-tuto';
+export class AppComponent implements OnInit{
+
+  ngOnInit(): void {
+    
+    const observer = {
+      next : (item : unknown) => console.log(`Une boite arrive ${item}`),
+      error : (err : unknown) => console.log(`Erreur ${err}`),
+      complete : () => console.log('termine ... plus rien'),
+    }
+
+    const stream = new Observable(myObserve => {
+      myObserve.next('Boite N1');
+      myObserve.next('Boite N2');
+      myObserve.next('Boite N3');
+      myObserve.complete();
+    });
+
+    stream.subscribe(observer);
+  }
 }
